554e9b5c96bd52bf2101f82135d9a4e244485343
pa1zore <pavanzore345@gmail.com>
2023-12-24 12:15:00 +0530

    First commit

--- src/main/java/UC1_user_registeration.java
+++ src/main/java/UC1_user_registeration.java
@@ -1,28 +1,8 @@
-import org.w3c.dom.ls.LSOutput;
-class User1
-{
-    private String firstName;
-    User1(String firstName)
-    {
-        this.firstName=firstName;
-    }
-    public Boolean isValidFirstName()
-    {
-        if(firstName.matches("[A-Z]{1,}[A-Za-z]{2,}"))
-        {
-            System.out.println("name is valid");
-            return true;
-        }
-        return false;
-    }
 
-
-}
 
 public class UC1_user_registeration {
     public static void main(String[] args) {
         System.out.println("welcome to user registration program");
-        User1 user= new User1("Pavan");
-        System.out.println(user.isValidFirstName());
+
     }
 }
--- src/test/java/UC1_ValidFirstNameTest.java
+++ src/test/java/UC1_ValidFirstNameTest.java
@@ -2,38 +2,5 @@ import org.junit.jupiter.api.*;
 
 public class UC1_ValidFirstNameTest {
 
-    @BeforeEach
-    void setup()
-    {
 
-    }
-    @AfterEach
-    void end()
-    {
-        System.out.println("test run successfully");
-    }
-    @Test
-    void should_return_true_after_giving_First_capital_alphabates()
-    {
-        //given
-        String str="Pavan";
-        //when
-        User1 user=  new User1(str);
-        Boolean actual=user.isValidFirstName();
-
-        //then
-        Assertions.assertTrue(actual);
-    }
-    @Test
-    void should_return_false_after_giving_first_capital_small()
-    {
-        //given
-        String str="pavan";
-        //when
-        User1 user=  new User1(str);
-        Boolean actual=user.isValidFirstName();
-
-        //then
-        Assertions.assertFalse(actual);
-    }
 }
